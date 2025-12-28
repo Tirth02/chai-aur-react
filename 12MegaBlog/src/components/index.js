8dacb527ae39ce6b094a4b2324552b82b3b55d4e
Tirth02 <pateltirth0902@gmail.com>
2025-12-28 15:46:28 -0500

    made pages and setup routing

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -2,13 +2,29 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
+import Button from "./Button";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE.JSX";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCard";
+import Protected from "./AuthLayout";
+import Input from "./Input";
+import Select from "./Select";
 export {
     Header,
     Footer,
     Container,
     Logo,
     LogoutBtn,
-    RTE
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    Protected,
+    Button,
+    Input,
+    Select
 }
